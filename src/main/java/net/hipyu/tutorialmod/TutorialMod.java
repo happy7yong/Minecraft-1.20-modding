d40825fb4a78e7301b6d38ff2bd6c8eb834d35d1
yongchaeyoung <happy7yong83@gmail.com>
2024-08-16 17:05:09 +0900

    feat : 배고픔 과 체력바 추가

--- src/main/java/net/hipyu/tutorialmod/TutorialMod.java
+++ src/main/java/net/hipyu/tutorialmod/TutorialMod.java
@@ -1,10 +1,14 @@
 package net.hipyu.tutorialmod;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
+import net.hipyu.tutorialmod.GUIoverlay.HealthGUI;
+import net.hipyu.tutorialmod.commands.ModCommands;
+import net.hipyu.tutorialmod.screenimage.ModEventHandlers;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -27,5 +31,19 @@ public class TutorialMod {
 
         // Register command events
         MinecraftForge.EVENT_BUS.register(ModCommands.class);
+
+        // 이벤트 버스에 리스너 등록
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::setup);
+        bus.addListener(this::doClientStuff);
+    }
+    private void setup(final FMLCommonSetupEvent event) {
+        // 서버 측 초기화 코드 (필요시)
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // 클라이언트 측 초기화 코드
+        // 여기에서 이벤트 리스너 등록
+        MinecraftForge.EVENT_BUS.register(new HealthGUI());
     }
 }
